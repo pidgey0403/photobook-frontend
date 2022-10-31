26e818da93a626ccdd4dfc39069bbbbd1c3b0c07
Gabby Niamat <gniamat@uwo.ca>
2022-10-30 21:08:38 -0400

    created typescript, react app with styled components. Forked MUI album template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Album from './Album';
+
+function App() {
+  return (
+    <div className="App">
+      <Album></Album>
+    </div>
+  );
+}
+
+export default App;
