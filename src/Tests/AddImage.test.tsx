3038b228e14a83f74c543eb3659b07ccf11bc10d
Gabby N <79562052+pidgey0403@users.noreply.github.com>
2022-10-31 16:52:53 -0400

    3 use graphql mutation to add functionality to add a memory button (#6)
    * added basic functionality for add image button, including text fields
    
    * added unit tests, created Tests folder and Components folder
    
    * enabled unit tests in CI.yml
    
    * added eslint and prettier CI tests in CI.yml
    
    * changed prettier check to only verify .tsx files

---
+++ src/Tests/AddImage.test.tsx
@@ -0,0 +1,8 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import AddImage from '../Components/AddImage';
+
+// render the entire component
+test('renders AddImage test', () => {
+    render(<AddImage />);
+});
