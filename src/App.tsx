0ee6219e31894df24442b3498a445e0d51f51c88
Gabby Niamat <gniamat@uwo.ca>
2022-10-30 21:25:14 -0400

    application cleanup, folder organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Album from './Album';
+import Album from './Components/Album';
 
 function App() {
   return (
--- src/Components/Album.tsx
+++ src/Components/Album.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -10,24 +8,9 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -99,7 +82,7 @@ export default function Album() {
                   <CardActions>
                     {/* TODO: click on it will show the full details: image + descrip */}
                     <Button size="small">More</Button>
-                    {/*  */}
+                    {/* TODO: add delete mutation ONLY if password is correct*/}
                     <Button size="small">Delete</Button>
                   </CardActions>
                 </Card>
