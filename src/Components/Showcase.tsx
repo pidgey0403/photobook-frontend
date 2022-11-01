82f7c29f34540580b46befeac7121b350b682a57
Gabby Niamat <gniamat@uwo.ca>
2022-10-31 21:42:15 -0400

    added props to Album, More, Showcase components for fetching database information

---
+++ src/Components/Showcase.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export interface ShowcaseProps {
+    title: string;
+    author: string;
+    date: string;
+    description: string;
+    image: string;
+}
+
+const Showcase: React.FC<ShowcaseProps> = ({
+    title = '',
+    author = '',
+    date = '',
+    description = '',
+    image = '',
+}: ShowcaseProps) => {
+    return (
+        <div style={{ padding: 20 }}>
+            <img
+                style={{ margin: 'auto', display: 'block' }}
+                src={`${image}?&auto=format`}
+                srcSet={`${image}?w=400&fit=crop&auto=format&dpr=2 2x`}
+                alt={title}
+                loading="lazy"
+            />
+
+            <Typography style={{ wordWrap: 'break-word', textAlign: 'left' }}>
+                <h2>{title}</h2>
+                By: {author} <br />
+                Date: {date} <br />
+                Description: {description} <br />
+                Full resolution: {image}
+            </Typography>
+        </div>
+    );
+};
+
+export default Showcase;
