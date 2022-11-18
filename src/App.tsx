abb9b5155dbca2fd55a9d3f521ecd7ccb6a55566
Gabby N <79562052+pidgey0403@users.noreply.github.com>
2022-11-18 17:37:37 -0500

    bootstrapped the app with apollo-client, created a dummy query to test functionality (#13)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Album from './Components/Album';
+import Example from './hook/example';
 
 function App() {
     return (
         <div className="App">
             <Album />
+            {/* Render out the GraphQL hook as a component */}
+            <Example />
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 // import reportWebVitals from './reportWebVitals';
 
+// Create a new ApolloClient object and pass in backend link
+const client = new ApolloClient({
+    uri: 'https://photobook-be.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+
 root.render(
-    <React.StrictMode>
+    // Bootstrap the application with ApolloClient
+    <ApolloProvider client={client}>
         <App />
-    </React.StrictMode>,
+    </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
