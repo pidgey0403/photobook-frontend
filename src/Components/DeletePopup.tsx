1e23dd298e839c0e935a63c56ba6813c9ece2a3f
Gabby Niamat <gniamat@uwo.ca>
2022-11-18 22:53:36 -0500

    added deletion mutation + password env variable

--- src/Components/DeletePopup.tsx
+++ src/Components/DeletePopup.tsx
@@ -5,10 +5,31 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
+import { gql, useMutation } from '@apollo/client';
 
-export default function DeletePopup() {
+// Define mutation to delete a specified Image
+const DELETE_IMAGE = gql`
+    mutation RemoveImage($id: Int!) {
+        removeImage(id: $id) {
+            author
+            date
+            description
+            file
+            id
+            likes
+            title
+        }
+    }
+`;
+
+export interface DelProps {
+    photoID: number;
+}
+
+const DeletePopup: React.FC<DelProps> = ({ photoID }: DelProps) => {
     const [open, setOpen] = React.useState(false);
-    const [error, setErr] = React.useState(false);
+    const [err, setErr] = React.useState(false);
+    const [deleteImage, { data, loading, error }] = useMutation(DELETE_IMAGE);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -18,18 +39,24 @@ export default function DeletePopup() {
         const userInput = (document.getElementById('name') as HTMLInputElement)
             .value;
         if (genre === 'Cancel') setOpen(false);
-        else if (userInput !== '123p') {
-            //TODO: add password encryption + verification
+        else if (userInput !== process.env.PASSWORD) {
             setErr(true);
         } else {
-            //correct password
+            // Password was valid
             setOpen(false);
-            //TODO: add graphql mutation to delete image from db + re-render application
+            deleteImage({
+                variables: {
+                    id: photoID,
+                },
+            });
             setTimeout(() => {
                 setErr(false);
             }, 1000);
         }
     };
+
+    if (loading) console.log('Submitting...');
+    if (error) console.log(`Submission error! ${error.message}`);
 
     return (
         <div>
@@ -37,7 +64,7 @@ export default function DeletePopup() {
             <Dialog open={open} onClose={handleClose}>
                 <DialogContent>
                     <DialogContentText>
-                        {error
+                        {err
                             ? `The provided password was incorrect, please try again.`
                             : `To delete this image, please enter the secure key given to you.`}
                     </DialogContentText>
@@ -62,4 +89,6 @@ export default function DeletePopup() {
             </Dialog>
         </div>
     );
-}
+};
+
+export default DeletePopup;
