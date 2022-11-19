88e2ba2e30034555af3789b1b8278dd94f6e1f50
Gabby Niamat <gniamat@uwo.ca>
2022-11-18 21:01:22 -0500

    added apollo-client query to get list of all Images

--- src/Components/Album.tsx
+++ src/Components/Album.tsx
@@ -13,10 +13,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import DeletePopup from './DeletePopup';
 import AddImage from './AddImage';
 import More from './More';
-import myJson from '../Temporary Data/database.json';
 import Heart from './Heart';
-
-//TODO: populate with GraphQL query
+import { useQuery, gql } from '@apollo/client';
 
 const theme = createTheme({
     palette: {
@@ -24,7 +22,35 @@ const theme = createTheme({
     },
 });
 
+// Write out the query using apollo-client
+const GET_IMAGES = gql`
+    query Images {
+        images {
+            author
+            date
+            description
+            file
+            id
+            likes
+            title
+        }
+    }
+`;
+
 export default function Album() {
+    const { loading, error, data } = useQuery(GET_IMAGES);
+    const [imageDict, setImageDict] = React.useState({ images: [] });
+
+    React.useEffect(() => {
+        if (loading === false && data) {
+            setImageDict(data);
+        }
+    }, [loading, data]);
+
+    // Add a nice loading screen?
+    if (loading) return <p>Loading...</p>;
+    if (error) console.log(`Error! ${error.message}`);
+
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
@@ -67,43 +93,55 @@ export default function Album() {
                 </Box>
                 <Container sx={{ py: 8 }} maxWidth="md">
                     <Grid container spacing={4}>
-                        {myJson.photos.map((card: any) => (
-                            <Grid item key={card.photoID} xs={12} sm={6} md={4}>
-                                <Card
-                                    sx={{
-                                        height: '100%',
-                                        width: '100%',
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                    }}
-                                >
-                                    <CardMedia
-                                        component="img"
-                                        image={card.image}
-                                        alt={card.title}
-                                    />
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography variant="h5" component="h2">
-                                            {card.title}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        {/* TODO: click on it will show the full details: image + descrip */}
-                                        <More
-                                            title={card.title}
-                                            author={card.author}
-                                            date={card.date}
-                                            description={card.description}
-                                            image={card.image}
+                        {imageDict.images.map(
+                            (card: {
+                                author: string;
+                                date: string;
+                                description: string;
+                                file: string;
+                                id: number;
+                                likes: number;
+                                title: string;
+                            }) => (
+                                <Grid item key={card.id} xs={12} sm={6} md={4}>
+                                    <Card
+                                        sx={{
+                                            height: '100%',
+                                            width: '100%',
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                        }}
+                                    >
+                                        <CardMedia
+                                            component="img"
+                                            image={card.file}
+                                            alt={card.title}
                                         />
+                                        <CardContent sx={{ flexGrow: 1 }}>
+                                            <Typography
+                                                variant="h5"
+                                                component="h2"
+                                            >
+                                                {card.title}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <More
+                                                title={card.title}
+                                                author={card.author}
+                                                date={card.date}
+                                                description={card.description}
+                                                image={card.file}
+                                            />
 
-                                        {/* TODO: add delete mutation ONLY if password is correct*/}
-                                        <DeletePopup />
-                                        <Heart countLike={card.count} />
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
+                                            {/* TODO: add delete mutation ONLY if password is correct*/}
+                                            <DeletePopup />
+                                            <Heart countLike={card.likes} />
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ),
+                        )}
                     </Grid>
                 </Container>
             </main>
