e3a2da6f84c8c31f677dfaf5d4e8f05701afe40b
Gabby Niamat <gniamat@uwo.ca>
2022-11-18 22:25:31 -0500

    created update likes mutation and added prop to Heart component

--- src/Components/Heart.tsx
+++ src/Components/Heart.tsx
@@ -1,25 +1,60 @@
 import * as React from 'react';
 import Badge from '@mui/material/Badge';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { gql, useMutation } from '@apollo/client';
 
 export interface HeartProps {
     countLike: number;
+    photoID: number;
 }
 
-const Heart: React.FC<HeartProps> = ({ countLike = 0 }: HeartProps) => {
+// Define mutation to update a single Image
+const INCREMENT_LIKES = gql`
+    mutation IncrementCounter($updateImageInput: UpdateImageInput!) {
+        updateImage(updateImageInput: $updateImageInput) {
+            author
+            date
+            description
+            file
+            id
+            likes
+            title
+        }
+    }
+`;
+
+const Heart: React.FC<HeartProps> = ({ countLike, photoID }: HeartProps) => {
     const [count, setCount] = React.useState(countLike);
     const [active, setActive] = React.useState(true);
+    const [increaseLikes, { data, loading, error, reset }] =
+        useMutation(INCREMENT_LIKES);
 
     const handleBadgeNum = () => {
         setActive(!active);
         if (active == true) {
-            // do POST operation to increase amount of likes stored
             setCount(count + 1);
+            // call the mutation and pass in appropriate Image data
+            increaseLikes({
+                variables: {
+                    updateImageInput: { id: photoID, likes: count + 1 },
+                },
+            });
         } else {
-            // do POST operation to decrease amount of likes stored
             setCount(count - 1);
+            reset(); // reset mutations cache
+            increaseLikes({
+                // decrement value of likes by 1
+                variables: {
+                    updateImageInput: { id: photoID, likes: count - 1 },
+                },
+            });
         }
     };
+
+    if (loading) console.log('Submitting...');
+    if (error) console.log(`Submission error! ${error.message}`);
+
+    console.log(data, loading, error);
 
     return (
         <div>
